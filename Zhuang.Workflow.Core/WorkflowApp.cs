88e3a5e7d0ca91082117d3a820ea024eaae48bf8
zhuangweibin <binweizhuang@hotmail.com>
2015-11-23 17:43:44 +0800

    aaa

--- Zhuang.Workflow.Core/WorkflowApp.cs
+++ Zhuang.Workflow.Core/WorkflowApp.cs
@@ -7,6 +7,21 @@ namespace Zhuang.Workflow.Core
 {
     public class WorkflowApp
     {
+        public string Id { get; set; }
 
+        public void Load(string instanceId)
+        {
+
+        }
+
+        public void Persist()
+        {
+
+        }
+
+        public void Run()
+        {
+
+        }
     }
 }
